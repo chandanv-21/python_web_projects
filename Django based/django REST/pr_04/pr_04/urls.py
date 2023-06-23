81661d9d500520e62da87761b4eeeb1947bfe533
chandanv-21 <chandanvishwakarma21@gmail.com>
2023-06-23 17:10:27 +0530

    first commit

--- Django based/django REST/pr_04/pr_04/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('student_api/', views.student_api),
-    path('student_api/<int:pk>', views.student_api),
-]
